61d99ae0053f4ee24956e11cb5a56b67fcbfdc15
Jari Porola <jari.porola@gofore.com>
2018-04-07 12:41:16 +0300

    count select with slider

--- who/config.js
+++
@@ -1,4 +0,0 @@
-const urlParams = new URLSearchParams(location.search);
-const count = parseInt(urlParams.get('count'));
-
-export const guessCount = count && count >= 3 && count <= 9 ? count : 3;
--- who/domUtils.js
+++ who/domUtils.js
@@ -1,3 +1,10 @@
 export const setText = (elementId, text) => {
     document.getElementById(elementId).textContent = text;
 }
+
+export const removeAllChilds = (elementId) => {
+    const element = document.getElementById(elementId);
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
