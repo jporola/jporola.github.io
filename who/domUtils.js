a13d7830901e247fd0cb4c8b5037e5339498ee65
Jari Porola <jari.porola@gofore.com>
2018-04-07 20:05:28 +0300

    dynamic page change

--- who/domUtils.js
+++ who/domUtils.js
@@ -1,10 +1,18 @@
 export const setText = (elementId, text) => {
     document.getElementById(elementId).textContent = text;
 }
+export const setError = (text) => {
+    parent.document.getElementById('error').textContent = text;
+}
 
 export const removeAllChilds = (elementId) => {
     const element = document.getElementById(elementId);
     while (element.firstChild) {
         element.removeChild(element.firstChild)
     }
+}
+
+export const loadPage = (name) => {
+    const cachebust = Date.now();
+    parent.document.page.data = `${name}.html?${cachebust}`;
 }
