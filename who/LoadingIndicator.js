f78e98f8876c2dc8f7fec8705bdd374177befe0e
Jari Porola <jari.porola@gofore.com>
2018-04-14 16:56:11 +0300

    style improvements

--- who/LoadingIndicator.js
+++ who/LoadingIndicator.js
@@ -1,5 +1,6 @@
 const containerId = 'loadingIndicatorContainer'
 const elementId = 'loadingIndicator'
+const elementIdInner = 'loadingIndicatorInner'
 
 export default class {
     static show() {
@@ -11,7 +12,10 @@ export default class {
         container.id = containerId
         const element = document.createElement('div')
         element.id = elementId
+        const elementInner = document.createElement('div')
+        elementInner.id = elementIdInner
         container.appendChild(element)
+        container.appendChild(elementInner)
         parent.document.body.appendChild(container)
     }
 
